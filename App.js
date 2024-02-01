ac57d36e81bc2093c2b8650207c0409ff3976862
Ini Jones <jonesinim@gmail.com>
2024-02-01 19:58:19 +0100

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import colors from "./app/config/colors";
 import HomeScreen from "./app/screens/HomeScreen";
+import CoffeeDetailsScreen from "./app/screens/CoffeeDetailsScreen";
+import coffees from './app/config/coffees';
 const App = () => {
   return (
     <View style={{ flex: 1, backgroundColor: colors.dark }}>
-      <HomeScreen />
+      {<HomeScreen />}
+
     </View>
   );
 };
